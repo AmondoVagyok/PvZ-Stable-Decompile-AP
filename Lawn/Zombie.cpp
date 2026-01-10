0cd48df309e28f1749990d058869947423e38d4a
InLiothixi <inliothixieOfficial@gmail.com>
2026-01-10 20:31:59 +0800

    Implemented Tidbits Zombatar
    (cherry picked from commit bb60fc19fd302e0cbabf1bb9505cc7fff0b8f7bb)

--- Lawn/Zombie.cpp
+++ Lawn/Zombie.cpp
@@ -191,6 +191,7 @@ void Zombie::ZombieInitialize(int theRow, ZombieType theType, bool theVariant, Z
     mSquashHeadColumn = -1;
 
     mIsAllergicToNuts = false;
+    mZombatarTidbitID = ReanimationID::REANIMATIONID_NULL;
     mZombatarHairID = ReanimationID::REANIMATIONID_NULL;
     mZombatarHairLineID = ReanimationID::REANIMATIONID_NULL;
     mZombatarEyeWearID = ReanimationID::REANIMATIONID_NULL;
@@ -7400,6 +7401,11 @@ void Zombie::Draw(Graphics* g)
         }
     }
 
+    if (mZombatarTidbitID != REANIMATIONID_NULL)
+    {
+        Reanimation* aReanim = mApp->ReanimationTryToGet(mZombatarTidbitID);
+        aReanim->Draw(g);
+    }
     if (mZombatarHairID != REANIMATIONID_NULL)
     {
         Reanimation* aReanim = mApp->ReanimationTryToGet(mZombatarHairID);
