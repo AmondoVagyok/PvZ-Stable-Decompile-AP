83336991b750fe1f2706a1cb5276d01fcf70f559
InLiothixi <inliothixieOfficial@gmail.com>
2026-01-11 18:23:08 +0800

    PNG are now prioritized over jpeg Fixed Fast Stretches
    (cherry picked from commit 1c66dac1934949ecdcf9f4dde21234fd070fa9f9)

--- SexyAppFramework/WidgetManager.cpp
+++ SexyAppFramework/WidgetManager.cpp
@@ -154,7 +154,7 @@ void WidgetManager::FlushDeferredOverlayWidgets(int theMaxPriority)
 					Graphics g(*mCurG);
 					g.Translate(-mMouseDestRect.mX, -mMouseDestRect.mY);
 					g.Translate(aWidget->mX, aWidget->mY);
-					g.SetFastStretch(!g.Is3D());
+					//g.SetFastStretch(!g.Is3D());
 					g.SetLinearBlend(g.Is3D());
 
 					aWidget->DrawOverlay(&g, aPriority);
@@ -452,7 +452,7 @@ bool WidgetManager::DrawScreen()
 			if ((aWidget->mDirty) && (aWidget->mVisible))
 			{
 				Graphics aClipG(g);
-				aClipG.SetFastStretch(!is3D);
+				//aClipG.SetFastStretch(!is3D);
 				aClipG.SetLinearBlend(is3D);
 				aClipG.Translate(aWidget->mX, aWidget->mY);				
 				aWidget->DrawAll(&aModalFlags, &aClipG);
