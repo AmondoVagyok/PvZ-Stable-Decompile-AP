218ed004fac6088032b24c7fe84a33063d345684
Victor Tran <vicr12345@gmail.com>
2026-03-12 16:30:52 +1100

    Add main.pak onboarding

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include "Sexy.TodLib/TodStringFile.h"
 #include "GameConstants.h"
 
+extern "C" {
+#include <tinyfiledialogs.h>
+}
+
+#include <filesystem>
+
 using namespace Sexy;
 
 bool (*gAppCloseRequest)();				//[0x69E6A0]
@@ -17,6 +23,61 @@ static std::string GetExeDirectory() {
 	PathRemoveFileSpecA(exePath);
 
 	return std::string(exePath);
+}
+
+bool PrepareMainPak()
+{
+	// Locate the PvZ main.pak file
+	std::string mainPakDir;
+	if (Sexy::FileExists("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Plants Vs Zombies\\main.pak"))
+	{
+		mainPakDir = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Plants Vs Zombies";
+	}
+	
+	if (mainPakDir.empty())
+	{
+		MessageBoxA(nullptr, "Please locate the main.pak file from your legally acquired copy of Plants vs. Zombies: Game of the Year edition.", "Game Setup", MB_OK | MB_ICONINFORMATION);
+		
+		const char* filterPatterns[] = { "main.pak" };
+
+		const char* main_pak_path = tinyfd_openFileDialog(
+			"Find main.pak",
+			NULL,
+			2,
+			filterPatterns,
+			"main.pak",
+			0
+		);
+		
+		if (!main_pak_path)
+		{
+			return false;
+		}
+		
+		mainPakDir = Sexy::GetFileDir(main_pak_path);
+	}
+	
+	auto mainPakFileSize = std::filesystem::file_size(mainPakDir + "\\main.pak");
+	if (mainPakFileSize != 45453180 && mainPakFileSize != 44925610) {
+		MessageBoxA(nullptr, "The main.pak file is invalid.", "Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+	
+	std::error_code ec;
+	std::filesystem::copy(mainPakDir + "\\properties", "properties", std::filesystem::copy_options::recursive | std::filesystem::copy_options::overwrite_existing, ec);
+	if (ec)
+	{
+		MessageBoxA(nullptr, "Could not copy Properties directory.", "Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+	std::filesystem::copy(mainPakDir + "\\main.pak", "main.pak", ec);
+	if (ec)
+	{
+		MessageBoxA(nullptr, "Could not copy main.pak file.", "Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+	
+	return true;
 }
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
@@ -42,6 +103,14 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	gGetCurrentLevelName = LawnGetCurrentLevelName;
 	gAppCloseRequest = LawnGetCloseRequest;
 	gAppHasUsedCheatKeys = LawnHasUsedCheatKeys;
+	
+	if (!Sexy::FileExists("main.pak"))
+	{
+		if (!PrepareMainPak())
+		{
+			return 1;
+		}
+	}
 
 	gLawnApp = new LawnApp();
 	std::string exeDir = GetExeDirectory();
